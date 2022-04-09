de3fdeae1a8d4d7aefffc8546889f5beee61ff1b
Andrea Jemmett <1787979+acidghost@users.noreply.github.com>
2022-04-09 08:16:16 +0200

    add `NyxConfig::set_workdir_path`

--- libnyx/src/lib.rs
+++ libnyx/src/lib.rs
@@ -83,6 +83,10 @@ impl NyxConfig {
         &self.config.fuzz.workdir_path
     }
 
+    pub fn set_workdir_path(&mut self, path: String) {
+        self.config.fuzz.workdir_path = path;
+    }
+
     pub fn dict(&self) -> Vec<Vec<u8>> {
         self.config.fuzz.dict.clone()
     }
